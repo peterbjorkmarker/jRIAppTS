df8ca6639a120820fcd6d8332006cb7e3af66d85
Maxim V. Tsapov <lptscsi@gmail.com>
2014-02-01 11:22:05 +0400

    spaDemo update

--- RIAppDemo/RIAppDemo/Scripts/RIAppDemo/SPA/SPAConfig.js
+++ RIAppDemo/RIAppDemo/Scripts/RIAppDemo/SPA/SPAConfig.js
@@ -1,45 +1,47 @@
-﻿require.config({
-    baseUrl: '/Scripts/RIAppDemo/SPA',
-    waitSeconds: 60,
-    paths: {
-    },
-    shim: {
-    }
-});
+define(["require", "exports", "spaDemoApp"], function(require, exports, SPADEMO) {
+    'use strict';
 
-require(['spaDemoApp'
-], function (SPADEMO) {
-    SPADEMO.mainOptions = RIAPP.global.utils.extend(false, SPADEMO.mainOptions, window.mainOptions);
+    
+
+    requirejs.config({
+        baseUrl: '/Scripts/RIAppDemo/SPA',
+        waitSeconds: 60,
+        paths: {},
+        shim: {}
+    });
+
+    SPADEMO.mainOptions = RIAPP.global.utils.extend(false, SPADEMO.mainOptions, mainOptions);
 
     //create and start application here
     RIAPP.global.addOnLoad(function (sender, a) {
-        var global = sender, mainOptions = SPADEMO.mainOptions;
+        var global = sender, appOptions = SPADEMO.mainOptions;
 
         //initialize images folder path
-        global.defaults.imagesPath = mainOptions.images_path;
+        global.defaults.imagesPath = appOptions.images_path;
+
         //create and then start application
-        var thisApp = new SPADEMO.DemoApplication(mainOptions);
+        var thisApp = new SPADEMO.DemoApplication(appOptions);
 
-        thisApp.registerTemplateGroup('custGroup',
-            {
-                url: mainOptions.spa_template1_url,
-                names: ["SPAcustTemplate", "goToInfoColTemplate", "SPAcustDetailTemplate", "customerEditTemplate", "customerDetailsTemplate", "orderEditTemplate",
-                    "orderDetEditTemplate", "orderDetailsTemplate", "productTemplate1", "productTemplate2",
-                    "prodAutocompleteTemplate"]
-            });
+        thisApp.registerTemplateGroup('custGroup', {
+            url: appOptions.spa_template1_url,
+            names: [
+                "SPAcustTemplate", "goToInfoColTemplate", "SPAcustDetailTemplate", "customerEditTemplate", "customerDetailsTemplate", "orderEditTemplate",
+                "orderDetEditTemplate", "orderDetailsTemplate", "productTemplate1", "productTemplate2",
+                "prodAutocompleteTemplate"]
+        });
 
-        thisApp.registerTemplateGroup('custInfoGroup',
-            {
-                url: mainOptions.spa_template2_url,
-                names: ["customerInfo", "salespersonTemplate1", "salespersonTemplate2", "salePerAutocompleteTemplate"]
-            });
+        thisApp.registerTemplateGroup('custInfoGroup', {
+            url: appOptions.spa_template2_url,
+            names: ["customerInfo", "salespersonTemplate1", "salespersonTemplate2", "salePerAutocompleteTemplate"]
+        });
 
-        thisApp.registerTemplateGroup('custAdrGroup',
-            {
-                url: mainOptions.spa_template3_url,
-                names: ["customerAddr", "addressTemplate", "addAddressTemplate", "linkAdrTemplate", "newAdrTemplate"]
-            });
+        thisApp.registerTemplateGroup('custAdrGroup', {
+            url: appOptions.spa_template3_url,
+            names: ["customerAddr", "addressTemplate", "addAddressTemplate", "linkAdrTemplate", "newAdrTemplate"]
+        });
 
-        thisApp.startUp(function (app) { });
+        thisApp.startUp(function (app) {
+        });
     });
 });
+//# sourceMappingURL=SPAConfig.js.map
